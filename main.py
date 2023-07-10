6a0496ac9279b7a4cbd632293ebcd73d0bba8049
ykundesu <49261069+ykundesu@users.noreply.github.com>
2023-07-10 18:28:22 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import os
 print("library loaded")
 def login(evname):
     token = os.environ.get(evname,None)
-    peinr(evname+":"+token+":a")
+    print(evname+":"+token+":a")
     if token == None:
         print("トークンなし:"+evname)
         a()
